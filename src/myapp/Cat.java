72dcc4b7c5ac6cd2fc5cf7a7c2925eedcaad482c
Chatchawan Sama <chatchawan.sam@ku.th>
2022-06-27 12:13:26 +0700

    intial commit : add Cat

---
+++ src/myapp/Cat.java
@@ -0,0 +1,12 @@
+package myapp;
+
+public class Cat {
+    String name;
+    String food;
+    public Cat(String name, String food) {
+        this.name = name;
+        this.food = food;
+    }
+
+    
+}
